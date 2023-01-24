f8b7dbb66141f0b1cd2194ad646b1a6d2df2bfa8
josiahcameron <josiahcameron2015@gmail.com>
2023-01-23 23:01:50 -0500

    Clock's Background Color Updates

--- main.js
+++ main.js
@@ -1,3 +1,59 @@
+//Solution:
+(function() {
+    "use strict"; //give an iffy that limits scope
+
+    const $display = document.querySelector(".clock-display");
+    const $progressBar = document.querySelector(".clock-progress-bar");
+    const $clock = document.querySelector(".clock");
+
+    let isHovering = false;
+
+    $clock.addEventListener("mouseover", function(){
+        isHovering = true;
+    });
+
+    $clock.addEventListener("mouseout", function(){
+        isHovering = false;
+    });
+
+    setInterval(function() {
+        const currentTime = new Date();
+        let seconds, minutes, hour; 
+        let hexseconds, hexminutes, hexhour;
+
+        if(isHovering){
+            seconds =  ('0' + currentTime.getSeconds().toString(16)).slice(-2); //toString turns it into a string and base 16 for hexadecimal
+            minutes =  ('0' + currentTime.getMinutes().toString(16)).slice(-2);
+            hour = ('0' + currentTime.getHours().toString(16)).slice(-2);
+
+            $clock.style.backgroundColor = `#${hour}:${minutes}:${seconds}`;
+
+        } else {
+            seconds =  ('0' + currentTime.getSeconds()).slice(-2);
+            minutes =  ('0' + currentTime.getMinutes()).slice(-2);
+            hour = ('0' + currentTime.getHours()).slice(-2);
+        }
+        hexseconds =  ('0' + currentTime.getSeconds().toString(16)).slice(-2); //toString turns it into a string and base 16 for hexadecimal
+        hexminutes =  ('0' + currentTime.getMinutes().toString(16)).slice(-2);
+        hexhour = ('0' + currentTime.getHours().toString(16)).slice(-2);
+
+        $clock.style.backgroundColor = `#${hexhour}${hexminutes}${hexseconds}`;
+        $progressBar.style.width = `${(seconds / 60) * 14}rem`;
+        $display.textContent = `${hour}:${minutes}:${seconds}`;
+    }, 1000);
+
+})();
+
+
+
+
+
+
+
+
+
+
+
 //DOM is a Web API - not a part of JavaScript. JavaScript just uses DOM to access the documents and their elements. The DOM is a collection, not a language.
 //A script allows you to use DOM directly in JS. It's a program run by a browser.
 //DOM is used by the program to change the webpage (html) by manipulating documents representing things on that page.
@@ -20,7 +76,7 @@ const paragraphs = document.querySelectorAll('p');
 
 
 //This function pulls the time based off of the date and local time zones. The 'new' operator returns the current date and time.
-
+/*
 const clock = setInterval(() =>{
     const time = document.querySelector(".clock-display");
     var date = new Date();
